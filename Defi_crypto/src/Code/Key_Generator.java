ae3ab6bd6b45fbf12600627665d0cafa9221fe81
Nicolas Gilbert <n-gilbert@laposte.net>
2016-11-30 01:06:02 -0500

    Correct Vigenere coding Update file interaction to support UTF-8 encoding Add Feistel class

--- Defi_crypto/src/Code/Key_Generator.java
+++ Defi_crypto/src/Code/Key_Generator.java
@@ -2,42 +2,42 @@ package Code;
 
 public class Key_Generator {
 
-	private static int[] S;
-	private static int output_space_size;
-	private static int cpt1;
-	private static int cpt2;
+    private static int[] S;
+    private static int output_space_size;
+    private static int cpt1;
+    private static int cpt2;
 
-	public static void initMask(int[] key) {
+    public static void initMask(int[] key) {
 
-		S = new int[256];
-		output_space_size = 256;
-		cpt1 = 0;
-		cpt2 = 0;
-		int j = 0;
+        S = new int[256];
+        output_space_size = 256;
+        cpt1 = 0;
+        cpt2 = 0;
+        int j = 0;
 
-		for (int i = 0; i < output_space_size; i++) {
-			S[i] = i;
-		}
+        for (int i = 0; i < output_space_size; i++) {
+            S[i] = i;
+        }
 
-		for (int i = 0; i < output_space_size; i++) {
-			j = (j + S[i] + key[i % key.length]) % output_space_size;
-			
-			int temp = S[i];
-			S[i] = S[j];
-			S[j] = temp;
-		}
-	}
+        for (int i = 0; i < output_space_size; i++) {
+            j = (j + S[i] + key[i % key.length]) % output_space_size;
 
-	public static int generateMask() {
+            int temp = S[i];
+            S[i] = S[j];
+            S[j] = temp;
+        }
+    }
 
-		cpt1 = (cpt1 + 1) % output_space_size;
-		cpt2 = (cpt2 + S[cpt1]) % output_space_size;
-		
-		int temp = S[cpt1];
-		S[cpt1] = S[cpt2];
-		S[cpt2] = temp;
+    public static int generateMask() {
 
-		return S[(S[cpt1] + S[cpt2]) % output_space_size];
-	}
+        cpt1 = (cpt1 + 1) % output_space_size;
+        cpt2 = (cpt2 + S[cpt1]) % output_space_size;
+
+        int temp = S[cpt1];
+        S[cpt1] = S[cpt2];
+        S[cpt2] = temp;
+
+        return S[(S[cpt1] + S[cpt2]) % output_space_size];
+    }
 
 }
--- Defi_crypto/src/Code/Vigenere.java
+++ Defi_crypto/src/Code/Vigenere.java
@@ -2,53 +2,54 @@ package Code;
 
 public class Vigenere {
 
-	private static int[] key;
-	private static int keyPosition;
+    private static int[] key;
+    private static int keyPosition;
 
-	public static void initVigenere(int[] keyToUse) {
-		key = keyToUse;
-		keyPosition = 0;
-	}
+    public static void initVigenere(int[] keyToUse) {
+        key = keyToUse;
+        keyPosition = 0;
+    }
 
-	private static int Encrypt(int message) {
+    public static int Encrypt(int message) {
 
-		int result;
+        int result;
 
-		if ((key[keyPosition] & 1) == 0) {
-			// +
-			result = (message + key[keyPosition]) % 256;
+        if ((key[keyPosition] & 1) == 0) {
+            // +
+            result = (message + key[keyPosition]) % 256;
 
-		} else {
-			// -
-			result = (message - key[keyPosition]);
-			if (result < 0) {
-				result += 256;
-			}
-		}
+        } else {
+            // -
+            result = (message - key[keyPosition]);
+            if (result < 0) {
+                result += 256;
+            }
+        }
 
-		keyPosition = (keyPosition + 2) % key.length;
-		return result;
-	}
+        keyPosition = (keyPosition + 1) % key.length;
+        return result;
+    }
 
-	private static int Decrypt(int cryptogramme) {
+    /*
+    public static int Decrypt(int cryptogramme) {
 
-		int result;
+        int result;
 
-		if ((key[keyPosition] & 1) == 0) {
-			// -
-			result = (cryptogramme - key[keyPosition]);
-			if (result < 0) {
-				result += 256;
-			}
+        if ((key[keyPosition] & 1) == 0) {
+            // -
+            result = (cryptogramme - key[keyPosition]);
+            if (result < 0) {
+                result += 256;
+            }
 
-		} else {
-			// +
-			result = (cryptogramme + key[keyPosition]) % 256;
-		}
+        } else {
+            // +
+            result = (cryptogramme + key[keyPosition]) % 256;
+        }
 
-		keyPosition = (keyPosition + 2) % key.length;
-		return result;
+        keyPosition = (keyPosition + 1) % key.length;
+        return result;
 
-	}
-
+    }
+     */
 }
